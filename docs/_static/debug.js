22e3f501824170813050002e17006b49e956ae5f
Jeremi Joslin <jeremi@newlogic.com>
2023-01-09 16:09:23 +0700

    Update pre-commit

--- docs/_static/debug.js
+++ docs/_static/debug.js
@@ -1,6 +1,7 @@
 // Add debug actions to flyout menu
 
 $(function () {
+    "use strict";
     $("[data-toggle='rst-debug-badge']").on("click", function () {
         $("[data-toggle='rst-versions']").toggleClass("rst-badge");
     });
