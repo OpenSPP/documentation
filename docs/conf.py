fcfe680da545fa56cfe42730d41453816b765a57
Jeremi Joslin <jeremi@newlogic.com>
2023-11-26 20:39:16 +0700

    update sitemap config

--- docs/conf.py
+++ docs/conf.py
@@ -351,7 +351,8 @@ html_title = "%(project)s v%(release)s" % {"project": project, "release": releas
 html_use_index = True
 
 # Used by sphinx_sitemap to generate a sitemap
-html_baseurl = "https://docs.openspp.org"
+html_baseurl = "https://docs.openspp.org/"
+sitemap_url_scheme = "{link}"
 
 # -- Options for HTML help output -------------------------------------------------
 
