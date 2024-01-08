035edce9a259bbd1f19c70732f5c7b9f8b3ec13a
Jeremi Joslin <jeremi@newlogic.com>
2024-01-08 17:00:18 +0700

    Update doc version

--- docs/conf.py
+++ docs/conf.py
@@ -132,9 +132,9 @@ year = str(now.year)
 # built documents.
 #
 # The short X.Y version.
-version = "1.0"
+version = "1.1"
 # The full version, including alpha/beta/rc tags.
-release = "1.0"
+release = "1.1"
 
 # -- General configuration ----------------------------------------------------
 
